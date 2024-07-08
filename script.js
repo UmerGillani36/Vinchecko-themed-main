fa968bae8c8ccbf9b8dbc3e47870ac1fae0c816f
aliyan-ansari <aliyanansari321@gmail.com>
2024-07-08 19:11:59 +0500

    tawk.to chat bot added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('pay-button').addEventListener('click', async function(e
     const phone = document.getElementById('phone_input').value.trim();
     const vin = document.getElementById('vin_input').value.trim();
     const packageElement = document.querySelector('input[name="package"]:checked');
-    
+
     // Validate form data
     if (!name || !email || !phone || !vin || !packageElement) {
         alert("Please fill in all the required fields and select a package.");
@@ -27,9 +27,15 @@ document.getElementById('pay-button').addEventListener('click', async function(e
         customerAddress: vin // Assuming VIN as address, adjust as necessary
     };
 
+    // Show loader and disable button
+    const loader = document.getElementById('loaders');
+    const payButton = document.getElementById('pay-button');
+    loader.style.display = 'block';
+    payButton.disabled = true;
+
     try {
         // Send request to backend
-        const response = await fetch('https://paypro-a7be355c2d61.herokuapp.com/api/create-payment', {
+        const response = await fetch('https://paypro-419fd75cfdeb.herokuapp.com/api/create-payment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -44,10 +50,17 @@ document.getElementById('pay-button').addEventListener('click', async function(e
         if (data[1].Click2Pay) {
             window.location.href = data[1].Click2Pay;
         } else {
+            if (data[0].status === "03") {
+                console.error('Error creating payment:', data[1].description);
+            }
             alert("Payment link could not be generated. Please try again.");
         }
     } catch (error) {
         console.error('Error creating payment:', error);
         alert("An error occurred. Please try again.");
+    } finally {
+        // Hide loader and enable button
+        loader.style.display = 'none';
+        payButton.disabled = false;
     }
 });
